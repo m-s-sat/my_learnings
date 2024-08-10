6ad1f1a3f765904fbd2fc18dd7b122cdbc5b3b0d
m-s-sat <mrinalsatyarthi2005@gmail.com>
2024-08-10 16:48:00 +0530

    learnings

--- imp_keyword.cpp
+++ imp_keyword.cpp
@@ -2,9 +2,11 @@
 #include<math.h>
 using namespace std;
 #define PI 3.14
+#define AREA(l,b) (l*b)
+#define AREA_1(r,h) (PI*pow(r,2)*h) 
 
 int main(){
-    int r;
-    cin>>r;
-    cout<<"area :- "<<PI*pow(r,2);
+    int r,l1,b1,r1,h;
+    cin>>r>>l1>>b1>>r1>>h;
+    cout<<"area of circle :- "<<PI*pow(r,2)<<" and area of square :- "<<AREA(l1,b1)<<" and area of cylinder :- "<<AREA_1(r1,h);
 }
