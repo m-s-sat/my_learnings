4b24e6ddc82974eda77f4a0cc1f2aaadac91fb65
m-s-sat <mrinalsatyarthi2005@gmail.com>
2024-09-07 18:30:45 +0530

    my learnings

--- separate_zero_rev.cpp
+++ separate_zero_rev.cpp
@@ -2,14 +2,11 @@
 using namespace std;
 
 void separet_zero(int arr[],int size){
-    for(int i=0;i<size;i++){
-        if(arr[i]==0){
-            for(int j=i+1;j<size;j++){
-                if(arr[j]!=0){
-                    swap(arr[i],arr[j]);
-                    break;
-                }
-            }      
+    int i=0;
+    for(int j=0;j<size;j++){
+        if(arr[j]!=0){
+            swap(arr[j],arr[i]);
+            i++;
         }
     }
 }
