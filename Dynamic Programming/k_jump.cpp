3c7d3a7751ebb579e64db785c95b6fcfb7c0ce84
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-06-11 21:57:02 +0530

    frog k jumps problem

--- Dynamic Programming/k_jump.cpp
+++ Dynamic Programming/k_jump.cpp
@@ -5,10 +5,9 @@ int solve(vector<int>heights,int n,int k){
     if(n==0) return 0;
     int min_energy = INT_MAX;
     for(int i=1;i<=k;i++){
-        if(n>=i){
+        if(i<=n){
             int jump = solve(heights,n-i,k)+abs(heights[n]-heights[n-i]);
-            cout<<"hello world"<<endl;
-            min_energy = min(jump,min_energy);
+            min_energy = min(min_energy,jump);
         }
     }
     return min_energy;
@@ -17,7 +16,7 @@ int solve(vector<int>heights,int n,int k){
 int main(){
     int n,k;
     cin>>n>>k;
-    vector<int> heights;
+    vector<int> heights(n);
     for(int i=0;i<n;i++) cin>>heights[i];
     cout<<solve(heights,n-1,k);
 }
