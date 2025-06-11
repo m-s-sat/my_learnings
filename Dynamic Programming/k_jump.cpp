5c77587581e96678c8ded4bf61bd125830e41b9f
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-06-11 22:02:24 +0530

    frog k jumps dp approach

--- Dynamic Programming/k_jump.cpp
+++ Dynamic Programming/k_jump.cpp
@@ -1,22 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int>heights,int n,int k){
+int solve(vector<int>heights,int n,int k,vector<int>dp){
     if(n==0) return 0;
     int min_energy = INT_MAX;
     for(int i=1;i<=k;i++){
         if(i<=n){
-            int jump = solve(heights,n-i,k)+abs(heights[n]-heights[n-i]);
+            if(dp[n]!=-1) return dp[n];
+            int jump = solve(heights,n-i,k,dp)+abs(heights[n]-heights[n-i]);
             min_energy = min(min_energy,jump);
         }
     }
-    return min_energy;
+    dp[n] = min_energy;
+    return dp[n];
 }
 
 int main(){
     int n,k;
     cin>>n>>k;
     vector<int> heights(n);
+    vector<int> dp(n+1,-1);
     for(int i=0;i<n;i++) cin>>heights[i];
-    cout<<solve(heights,n-1,k);
+    cout<<solve(heights,n-1,k,dp);
 }
